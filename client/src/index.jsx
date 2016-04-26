bec164f99b6655304cd9e81eac613b5f315a2d59
Jeremy Cox <jeremyrcox.823@gmail.com>
2016-04-25 23:12:07 -0400

    Added word list client side
    Add word list and random selector on client side

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,43 +3,17 @@ import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { fromJS } from 'immutable';
+import { Map } from 'immutable';
 
+import words from './words';
 import reducer from './reducer';
 import App from './components/App';
 import { GameContainer } from './components/Game';
 
-const sampleData = {
-  words: [
-    'omnichannel',
-    'epicenter',
-    'portfolio rationalization',
-    'synergy',
-    'goodness',
-    'awfulness',
-    'established patterns',
-    'parity',
-    'roadmap',
-    'bucket of work',
-    'paramount',
-    'architectural runway',
-    'flux capacitor',
-    'ramping up intelligently',
-    'differentiate',
-    'money where our mouth is',
-    'tap into these capabilities',
-    'rigidity',
-    'myriad',
-    'theme',
-    'manifest',
-    'stewards',
-    'catalyst',
-    'destiny',
-    'speculate',
-  ],
+const initialState = Map({
+  words: words(),
   score: 0,
-};
-const initialState = fromJS(sampleData);
+});
 
 const store = (
     window.devToolsExtension ? window.devToolsExtension()(createStore) : createStore
