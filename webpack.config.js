af257b0730b4118d6d6020446df2829d10274168
Jeremy Cox <jeremyrcox.823@gmail.com>
2016-07-07 22:24:39 -0400

    production updates
    removed hot-loader and added production config

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,50 @@
 var webpack = require('webpack');
 
 module.exports = {
-	entry: [
-		'webpack-dev-server/client?http://localhost:8080',
-    	'webpack/hot/only-dev-server',
-		'./src/index.jsx'
-	],
-	module: {
-		preLoaders: [
-	      {
-	        test: /\.jsx$|\.js$/,
-	        loader: 'eslint-loader',
-	        include: __dirname + '/src',
-	        exclude: /bundle\.js$/
-	      }
-	    ],
-		loaders: [{
-			test: /\.jsx?$/,
-			exclude: /node_modules/,
-			loader: 'react-hot!babel'
-		}
+    entry: [
+        './src/index.jsx'
+    ],
+    module: {
+        preLoaders: [
+          {
+            test: /\.jsx$|\.js$/,
+            loader: 'eslint-loader',
+            include: __dirname + '/src',
+            exclude: /bundle\.js$/
+          }
+        ],
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel'
+        }
       ]
-	},
-	resolve: {
-		extensions: ['', '.js', '.jsx']
-	},
-	output: {
-		path: __dirname + '/dist',
-		publicPath: '/',
-		filename: 'bundle.js'
-	},
-	devServer: {
-		contentBase: './dist',
-		hot: true
-	},
-	plugins: [
-	    new webpack.HotModuleReplacementPlugin()
-	]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+          compress: {
+            warnings: false,
+            screw_ie8: true
+          },
+          comments: false,
+          sourceMap: false
+      })
+    ]
 };
